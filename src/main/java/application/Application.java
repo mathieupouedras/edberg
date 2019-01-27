0a8246fe65431a7e4e41ed012bbc0342838a0da7
Mathieu Pouëdras <7620812+mathieupouedras@users.noreply.github.com>
2019-01-27 22:38:38 +0100

    implements basic secheduling

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -10,7 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 @SpringBootApplication
@@ -22,19 +27,39 @@ public class Application {
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext context) {
+
         return args -> {
             Repository repository = new PropertiesFileRepository("repository.data");
             CourtRepository courtRepository = new StaticCourtRepository();
             RequestService requestServiceMathieu = new HttpRequestService(repository);
             RequestService requestServiceJulien = new HttpRequestService(repository);
 
-            Booker booker = new DummyBooker();
+            LocalDateTime loginTime = LocalDateTime.of(2019, 1, 28, 23, 50, 0, 0);
 
-            boolean loginsAreSuccessfull = booker.loginUsers(repository, requestServiceMathieu, requestServiceJulien);
+            LocalDateTime bookingTime = LocalDateTime.of(2019, 1, 28, 0, 0, 0, 0);
 
-            if (loginsAreSuccessfull) {
-                booker.boo2Hours(repository, courtRepository, requestServiceMathieu, requestServiceJulien);
-            }
+            Booker booker = new EffectivBooker();
+
+            TimerTask timerTaskLogin = new TimerTask() {
+                @Override
+                public void run() {
+                    boolean loginsAreSuccessfull = booker.loginUsers(repository, requestServiceMathieu, requestServiceJulien);
+                }
+            };
+
+            ScheduledExecutorService executorServiceLogin = Executors.newSingleThreadScheduledExecutor();
+            executorServiceLogin.schedule(timerTaskLogin, LocalDateTime.now().until(loginTime, ChronoUnit.NANOS), TimeUnit.NANOSECONDS);
+
+            TimerTask timerTaskBooking = new TimerTask() {
+                @Override
+                public void run() {
+                    booker.boo2Hours(repository, courtRepository, requestServiceMathieu, requestServiceJulien);
+                }
+            };
+
+            ScheduledExecutorService executorServiceBooking = Executors.newSingleThreadScheduledExecutor();
+            executorServiceBooking.schedule(timerTaskBooking, LocalDateTime.now().until(bookingTime, ChronoUnit.NANOS), TimeUnit.NANOSECONDS);
+
         };
     }
 
--- src/main/java/domain/EffectivBooker.java
+++ src/main/java/domain/EffectivBooker.java
@@ -4,29 +4,36 @@ public class EffectivBooker implements Booker {
 
     @Override
     public void boo2Hours(Repository repository, CourtRepository courtRepository, RequestService requestServiceMathieu, RequestService requestServiceJulien) {
-        Court mouchotte = courtRepository.forName("Mouchotte");
-        Schedule mouchotte17 = new Schedule(mouchotte, "1020", 60, 3, "17:00", "18:00");
-        String date = "2019-01-30";
+        Court elisabeth7 = courtRepository.forName("Elisabeth n°7");
+        Court elisabeth8 = courtRepository.forName("Elisabeth n°8");
+        String date = "2019-02-03";
 
-        boolean bookingSuccessfull1 = book(repository, requestServiceMathieu, mouchotte, mouchotte17, date,
+        Schedule elisabeth7_10h = new Schedule(elisabeth7, "600", 60, 6, "10:00", "11:00");
+        Schedule elisabeth7_11h = new Schedule(elisabeth7, "660", 60, 6, "11:00", "12:00");
+
+        Schedule elisabeth8_10h = new Schedule(elisabeth8, "600", 60, 6, "10:00", "11:00");
+        Schedule elisabeth8_11h = new Schedule(elisabeth8, "660", 60, 6, "11:00", "12:00");
+
+        boolean bookingSuccessfull1 = book(repository, requestServiceMathieu, elisabeth7, elisabeth7_11h, date,
                 repository.getFormParameterName("form.parameter.member.emmanuel.value"));
 
         if (bookingSuccessfull1) {
-            Schedule mouchotte18 = new Schedule(mouchotte, "1080", 60, 3, "18:00", "19:00");
-            book(repository, requestServiceJulien, mouchotte, mouchotte18, date,
+            book(repository, requestServiceJulien, elisabeth7, elisabeth7_10h, date,
                     repository.getFormParameterName("form.parameter.member.damien.value"));
         }
         else {
-            Schedule mouchotte19 = new Schedule(mouchotte, "1140", 60, 3, "19:00", "20:00");
-            book(repository, requestServiceJulien, mouchotte, mouchotte19, date,
+            book(repository, requestServiceJulien, elisabeth8, elisabeth8_10h, date,
                     repository.getFormParameterName("form.parameter.member.damien.value"));
+
+            book(repository, requestServiceMathieu, elisabeth8, elisabeth8_11h, date,
+                    repository.getFormParameterName("form.parameter.member.emmanuel.value"));
 
         }
     }
 
-    private boolean book(Repository repository, RequestService requestServiceMathieu, Court mouchotte, Schedule mouchotte17, String date, String partnerId) {
-        Response responseSchedule = requestServiceMathieu.chooseSchedule(mouchotte, mouchotte17);
-        Response responseCreate = requestServiceMathieu.createSchedule(mouchotte17, date,
+    private boolean book(Repository repository, RequestService requestService, Court court, Schedule schedule, String date, String partnerId) {
+        Response responseSchedule = requestService.chooseSchedule(court, schedule);
+        Response responseCreate = requestService.createSchedule(schedule, date,
                 partnerId);
 
         if (!responseCreate.isSuccessFull()) {
