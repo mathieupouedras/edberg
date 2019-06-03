9bc983d38ed5f739ba626752ebb0be510125fba3
Mathieu Pouëdras <7620812+mathieupouedras@users.noreply.github.com>
2019-06-03 08:39:26 +0200

    failed booking for the 9th of may pâques

--- src/main/java/domain/EffectivBooker.java
+++ src/main/java/domain/EffectivBooker.java
@@ -6,7 +6,7 @@ public class EffectivBooker implements Booker {
     public void boo2Hours(Repository repository, CourtRepository courtRepository, RequestService requestServiceMathieu, RequestService requestServiceJulien) {
         Court elisabeth7 = courtRepository.forName("Elisabeth n°7");
         Court elisabeth8 = courtRepository.forName("Elisabeth n°8");
-        String date = "2019-05-26";
+        String date = "2019-06-09";
 
         Schedule elisabeth7_10h = new Schedule(elisabeth7, "600", 60, 6, "10:00", "11:00");
         Schedule elisabeth7_11h = new Schedule(elisabeth7, "660", 60, 6, "11:00", "12:00");
